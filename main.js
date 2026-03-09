268afa6c32a474d4b3fa102e5516feb0a5cdf692
Piyushify <piyushsharma3042007@gmail.com>
2026-03-09 07:34:06 +0530

    update

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log('JS Connected!');
+const cube = document.getElementById("cube");
+const placement = ["front","back","left","right","top","bottom"];
+function create() {
+  for (let i = 0; i < 6; i++) {
+    face = document.createElement("div");
+    face.classList.add("face");
+    cube.appendChild(face);
+  }
+  const faces = document.querySelectorAll(".face"); 
+  for (let i = 0; i < 6; i++) {
+    faces[i].classList.add(placement[i]);
+  }
+}
+create();
